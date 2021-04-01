c7603a47a19fca52458b53d7e7e271bc92480e76
almaz <almaz_73@mail.ru>
2021-04-01 15:08:50 +0300

    создание вызова

--- cypress/integration/Ambulance/Доступные сайты.js
+++ cypress/integration/Ambulance/Доступные сайты.js
@@ -8,14 +8,11 @@ describe('Доступность сайтов', () => {
         { name: 'prime:40080 (Казань)', link: 'prime:40080' },
         { name: '192.168.83.138:8080 (Якутия)', link: '192.168.83.138:8080' },
         { name: '192.168.83.53:5080 (Тест2 - Чувашия)', link: '192.168.83.53:5080' },
-        { name: '192.168.83.120:50280 (Тест - Кострома)', link: '192.168.83.120:50280' },
+        { name: '192.168.83.198:50280 (Тест - Кострома)', link: '192.168.83.198:50280' },
         { name: '192.168.83.120:28080 (Тест - Кемерова)', link: '192.168.83.120:28080' }
 
     ].forEach(element => {
         it(element.name, () => {
-            cy.log("====",element)
-            //cy.log(element.link)
-
             /*var ImageObject = new Image();
             ImageObject.src = "http://" + element.link + "/ambulance/resources/img/systemL.png?cachebreaker="+new Date().getTime();
             
@@ -25,10 +22,16 @@ describe('Доступность сайтов', () => {
                 cy.log("Ping failed :(");
             }*/
 
-         
+            // старый медленный подход
+            /*cy.visit('http://' + element.link + '/ambulance/login')
+                .title().should('eq', 'Вход в систему')*/ 
 
-           cy.visit('http://' + element.link + '/ambulance/login')
-                .title().should('eq', 'Вход в систему')
+            cy.request('http://' + element.link + '/ambulance/login')
+                .its('body')
+                .should('include', '<title>')
+                .and('include', 'Вход в систему')
+                .and('include', '</html>')
+
         })
     });
 })
--- cypress/integration/Ambulance/Создание вызова.js
+++ cypress/integration/Ambulance/Создание вызова.js
@@ -1,117 +1,104 @@
-const { debug } = require("console")
+//const { get } = require("http");
 
-let site = '192.168.83.53:6080'
+let site = "192.168.83.53:7080" 
+let token;
 
-Cypress.on('uncaught:exception', (err, runnable) => {
-    //alert(err)
-    // returning false here prevents Cypress from
-    // failing the test
-    cy.log("========", err)
-    return false
-})
+Cypress.on('uncaught:exception', () => false); // чтобы тест не падал на неожиданных ошибках
 
+Cypress.Cookies.defaults({ preserve: ['JSESSIONID', 'C9AB6D5557F3BDF397301269069FB649'] }); // борьба против повторных авторизаций
 
-context('Амибулансе', () => {
-    beforeEach(() => {
-        cy.viewport(1280, 720)
-        cy.visit('http://' + site + '/ambulance/login')
-        cy.get('input[placeholder="Логин"]').type("adminSMP")
-        cy.get('input[placeholder="Пароль"]').type("ambulance17")
-        cy.contains('Войти').click()
-        cy.title().should('eq', 'СМП 3.0')
-    })
+describe('Создание вызова 8080', function () {
+    beforeEach(function () {
+        cy.viewport(1280, 720);
+        cy.getCookie('JSESSIONID').then(cook => {
+            if (!cook || !cook.name) {
+                // Авторизация, если не авторизовано
+                cy.visit(`http://${site}/ambulance/login`)
+                cy.get('input[placeholder="Логин"]').type('adminSMP')
+                cy.get('input[placeholder="Пароль"]').type('ambulance17')
+                cy.contains('Войти').click()
+                cy.title().should('eq', 'СМП 3.0')
 
-    beforeEach(() => {
-        cy.visit('http://' + site + '/ambulance/#/callcard/') // перехожу на "Новый КТ"
-    })
-
-    it('Создание вызова', () => {
-
-     //   cy.wait(500).then(function () {
-     //       if (Cypress.$('.modal-content').length) {
-                cy.get('.modal-content button').then(div => {
-                    cy.log(' = = =  =div', div)
-                    if (div) { // Если выскакивает окно "Признак вызова", закрывает по кнопке Далее 
-                        cy.get('.modal-content button').contains('Далее').click()
-                    }
+                cy.getCookie('JSESSIONID').then(cookie => {
+                    console.log(' - - - запоминаем прежний токен, чтобы повторно не авторизоваться - - - ');
+                    token = cookie.value
                 })
+            }
 
-        //    }
+            cy.wait(300);
+            cy.visit('http://' + site + '/ambulance/#/callcard/')
+        })
+    })
+    beforeEach(function () {
+        if (!Cypress.$('#headerNavbar').length) {
+            cy.clearCookie(''); // чистим старый токен
+            cy.wait(500).then(() => window.location.reload());
+            console.log('- - - - - - - - - -почистили авторизацию, обновляемся- - - - - - - - - - - ',);
+            return;
+        }
+    })
 
-       // })
+    it('создание вызова для всех сайтов', function () {
+        cy.wait(300);
+        cy.get('body').then((body) => {
+            if (Cypress.$('.modal-open').length) {
+                cy.get('.modal-dialog').contains('СП/НП').then(modal => {
+                    // Если выскакивает окно "Признак вызова", закрывает по кнопке Далее 
+                    cy.get('.modal-content button').contains('Далее').click()
+                })
+            }
+        })
 
-
-        cy.get('chosen[ng-model="call.spnp.reasonType"]').click() // нажимаю на комбобокс повод
-            .then(() => {
-                cy.get('chosen[ng-model="call.spnp.reasonType"]').contains('падение с высоты').click() //выбираю повод "падение с высоты"
-            })
+       
+        cy.get('chosen[ng-model="call.spnp.reasonType"]')
+            .click() // нажимаю на комбобокс повод
+            .then(() => cy.get('chosen[ng-model="call.spnp.reasonType"]').contains('падение с высоты').click()) //выбираю повод "падение с высоты"
         cy.get('call-place [ng-model="$parent.callPlace"]').click() // нажимаю Тип места вызова
-        cy.get('call-place [ng-model="$parent.callPlace"] li:first').click() // выбираю первый элемент
-
-        cy.get('call-place [ng-model="address.cityAddress.mo"]').click() // выбираю МО места вызова
             .then(() => {
-                cy.get('call-place [ng-model="address.cityAddress.mo"] li:first').click() // выбираю первый элемент
-            })
+                cy.get('call-place [ng-model="$parent.callPlace"] li:first').click() // выбираю первый элемент
+            });
+        cy.get('call-place [ng-model="address.cityAddress.mo"]')
+            .click() // выбираю МО места вызова
+            .then(() => cy.get('call-place [ng-model="address.cityAddress.mo"] li:first').click()) // выбираю первый элемент            
         cy.get('button').contains('Сохранить').click(); // нажимаю сохранить вызов
 
 
-       // cy.wait(500).then(() => {
-        //    if (Cypress.$('.modal-dialog').length) {
-                cy.get('.modal-dialog').contains('Пожалуйста, выберите зону').then(modal => {
-                    if (modal) {
-                        cy.log('8888888')
-                        cy.get('[ng-model="call.zoneId"]').click()
-                            .then(() => {
-                                cy.get('[ng-model="call.zoneId"] li:first').click()
-                                cy.get('.modal-dialog').contains('Выбрать').click()
-                            })
-                    }
-                })
-    //        }
-    //    })
-
-
-       // cy.wait(1000).then(function () {
-    //        if (Cypress.$('.modal-dialog').length) {
-                cy.get('.modal-dialog').contains('Выберите повторный и/или задвоенный вызов').then(modal => {
-                    if (modal) {
-                        cy.get('.modal-dialog').contains('Продолжить').click()
-                    }
-                })
-  //          }
-       // })
-
-
-
-
-
-
-
-
-        cy.get('.modal-dialog').contains('Событие').then(modal => {
-
-            if (modal) {
-                cy.get('[ng-model="ctrl.ev.eventScaleId"]').click()
+        cy.wait(300);
+        cy.get('body').then((body) => {
+            if (body[0].innerText.indexOf('Пожалуйста, выберите зону') > -1) {
+                cy.get('[ng-model="call.zoneId"]').click()
                     .then(() => {
-                        cy.get('[ng-model="ctrl.ev.eventScaleId"] li:first').click()
-                        cy.get('.modal-dialog').contains('Сохранить').click()
+                        cy.get('[ng-model="call.zoneId"] li:first').click()
+                        cy.get('.modal-dialog').contains('Выбрать').click()
                     })
             }
         })
 
 
+        cy.wait(300);
+        cy.get('body').then((body) => {
+            if (body[0].innerText.indexOf('Выберите повторный и/или задвоенный вызов') > -1) {
+                cy.get('.modal-dialog').contains('Продолжить').click()
+            }
 
-        //cy.get('.modal-dialog button').contains('Продолжить').click()
+        })
+
+
+        cy.wait(300);
+        cy.get('body').then((body) => {
+            if (body.find('.modal-dialog').length > 0) {
+                if (body[0].innerText.indexOf('Событие') > -1) {
+                    cy.get('[ng-model="ctrl.ev.eventScaleId"]').click()
+                        .then(() => {
+                            cy.get('[ng-model="ctrl.ev.eventScaleId"] li:first').click()
+                            cy.get('.modal-dialog').contains('Сохранить').click()
+                        })
+                }
+            }
+        })
+
 
 
         cy.hash().should('match', /\d+/)
     })
-
 })
-
-
-
-
-
-
-
